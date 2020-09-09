d509b15c9f7679dfa6388535af20da76ad65b225
Hades68 <gluza.dom9@gmail.com>
2020-09-09 22:51:12 +0200

    Project control system in progress

---
+++ src/com/more_challenges/BiggerDecimal.java
@@ -0,0 +1,59 @@
+package com.more_challenges;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class BiggerDecimal {
+    public static void main(String[] args) {
+        List<BigDecimal> decimalList = new ArrayList<>();
+        Map<BigDecimal, String> mapToCopy = new HashMap<>();
+        String[] s = new String[11];
+        s[0] = "-100";
+        s[1] = "50";
+        s[2] = "0";
+        s[3] = "56.6";
+        s[4] = "90";
+        s[5] = "0.12";
+        s[6] = ".12";
+        s[7] = "02.34";
+        s[8] = "000.000";
+        s[9] = null;
+        s[10] = null;
+        String[] s2 = new String[9];
+        for (int i = 0; i < s2.length; i++) {
+            s2[i] = s[i];
+        }
+//        System.out.println(Arrays.toString(s2));
+        try {
+            for (String es : s2) {
+                decimalList.add(new BigDecimal(es));
+            }
+            for (int i = 0; i < s2.length; i++) {
+                mapToCopy.put(decimalList.get(i), s2[i]);
+            }
+            decimalList.sort(Comparator.reverseOrder());
+            for (int i = 0; i < s2.length; i++) {
+                s2[i] = mapToCopy.get(decimalList.get(i));
+            }
+            for (BigDecimal bd: decimalList) {
+                System.out.println(bd);
+            }
+            s = s2;
+//            for (String sss : s) {
+//                System.out.println(sss);
+//            }
+        } catch (NullPointerException e) {
+            System.out.println("SomethingWentWrong");
+        }
+
+    }
+}
+//-100
+//50
+//0
+//56.6
+//90
+//0.12
+//.12
+//02.34
+//000.000
